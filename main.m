929a120609a2f3d2a8eb6d1d5ae589cdc4dc97f4
arunthampi <arun.thampi@gmail.com>
2010-11-19 21:44:37 +0800

    First commit for Paperadio, mothafucka

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Paperadio
+//
+//  Created by Arun Thampi on 11/19/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
